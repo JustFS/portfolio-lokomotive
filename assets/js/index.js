8f28d2b2b96468df2905db30ece0a389de997180
Julien Az <julien.azbrg@gmail.com>
2022-08-17 17:24:00 +0200

    fin J2

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,12 @@ const scroll = new LocomotiveScroll({
   tablet: { smooth: true },
   smartphone: { smooth: true },
 });
+
+// Body color
+scroll.on("scroll", () => {
+  if (document.querySelector(".is-inview#color")) {
+    document.body.style.animation = "backBlack 1s ease-in-out forwards";
+  } else {
+    document.body.style.animation = "backWhite 1s ease-in-out forwards";
+  }
+});
